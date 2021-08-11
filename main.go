b51ee619ffd23713bf452d7a98925e8dbd6dbf2f
Shota Muto <dos9954@gmail.com>
2021-08-11 12:39:59 +0900

    add port-id-subtype option for remote

--- main.go
+++ main.go
@@ -17,12 +17,13 @@ var oids = map[string]string{
 	"lldpLocPortDesc": ".1.0.8802.1.1.2.1.3.7.1.4",
 	"lldpRemSysName":  ".1.0.8802.1.1.2.1.4.1.1.9",
 	"lldpRemPortId":   ".1.0.8802.1.1.2.1.4.1.1.7",
+	"lldpRemPortDesc": ".1.0.8802.1.1.2.1.4.1.1.8",
 }
 
 type lldpEntry struct {
-	LocalPortName string
-	RemotePortId  string
-	RemoteSysName string
+	LocalPortName  string
+	RemotePortName string
+	RemoteSysName  string
 }
 
 func main() {
@@ -30,6 +31,7 @@ func main() {
 	var community = flag.String("c", "public", "SNMP community")
 	var format = flag.String("o", "csv", "Output format (csv, json)")
 	var localPortType = flag.String("lt", "desc", "port-id-subtype selection for local (desc|id)")
+	var remotePortType = flag.String("rt", "desc", "port-id-subtype selection for remote (desc|id)")
 
 	flag.Parse()
 
@@ -52,14 +54,11 @@ func main() {
 
 	if *localPortType == "desc" {
 		results, err = target.BulkWalkAll(oids["lldpLocPortDesc"])
-		if err != nil {
-			log.Fatal(err)
-		}
 	} else if *localPortType == "id" {
 		results, err = target.BulkWalkAll(oids["lldpLocPortId"])
-		if err != nil {
-			log.Fatal(err)
-		}
+	}
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	for _, pdu := range results {
@@ -76,13 +75,18 @@ func main() {
 		lldpEntries[strings.Split(pdu.Name[26:], ".")[1]].RemoteSysName = sysName
 	}
 
-	results, err = target.BulkWalkAll(oids["lldpRemPortId"])
+	if *remotePortType == "desc" {
+		results, err = target.BulkWalkAll(oids["lldpRemPortDesc"])
+	} else if *remotePortType == "id" {
+		results, err = target.BulkWalkAll(oids["lldpRemPortId"])
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	for _, pdu := range results {
 		remotePort := string(pdu.Value.([]uint8))
-		lldpEntries[strings.Split(pdu.Name[26:], ".")[1]].RemotePortId = remotePort
+		lldpEntries[strings.Split(pdu.Name[26:], ".")[1]].RemotePortName = remotePort
 	}
 
 	if *format == "json" {
@@ -95,12 +99,12 @@ func main() {
 	} else if *format == "csv" {
 		fmt.Println("Local,RemotePort,RemoteSysName")
 		for _, lldp := range lldpEntries {
-			if lldp.RemotePortId == "" && lldp.RemoteSysName == "" {
+			if lldp.RemotePortName == "" && lldp.RemoteSysName == "" {
 				continue
 			}
 			fmt.Println(
 				lldp.LocalPortName, ",",
-				lldp.RemotePortId, ",",
+				lldp.RemotePortName, ",",
 				lldp.RemoteSysName,
 			)
 		}
